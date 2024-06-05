40f43f2b2e4c70c3e35d58e393a846f0370aea36
NoonMira <163841273+NoonMira@users.noreply.github.com>
2024-06-05 12:35:45 +0700

    feat: react form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message,setGreeting] = useState("")
+  const [show,setShow] = useState("Greeting Message")
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{show}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={message} onChange={(event)=>{setGreeting(event.target.value)}} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={()=>{setShow(message)}}>Update text</button>
       </div>
     </div>
   );
